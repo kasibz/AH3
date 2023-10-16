727f607401304b5238fc630237f24b36382ad674
kasib <kasibzxc@gmail.com>
2023-10-16 16:37:46 -0400

    'recursion' with no recursive case :)

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -1,27 +1,26 @@
 public class FizzBuzz {
 
-    private static int START = 1;
+    public static void fizzBuzz(int n) {
+        // Count up from the base case
+        // this way basically ignores the call stack
+        // no recursive case.
+        if (n >= 1) {
 
-    public static int fizzBuzz(int n) {
-        // if start = 1,
-        if (START > n) {
-            return 0;
-        } else {
-            if (START % 3 == 0 && START % 5 == 0)
+            fizzBuzz(n - 1);
+            if (n % 3 == 0 && n % 5 == 0)
                 System.out.println("FizzBuzz");
-            else if (START % 3 == 0)
+            else if (n % 3 == 0)
                 System.out.println("Fizz");
-            else if (START % 5 == 0)
+            else if (n % 5 == 0)
                 System.out.println("Buzz");
             else
-                System.out.println(START);
-            START++;
-            return fizzBuzz(n);
+                System.out.println(n);
+            return;
         }
     }
 
     public static void main(String[] args) {
-        System.out.print(fizzBuzz(15));
+        fizzBuzz(15);
         // System.out.print(fizzBuzz(30));
     }
 }
